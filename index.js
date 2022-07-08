895abc9a5b1e8e6feb8fff79a0c4dfa0108ac58e
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 07:56:36 +0200

    make-unordered-list

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-ReactDOM.render(<p>Hi, my name is Placide!</p>,document.getElementById("root"))
-// ReactDOM.render(
-// <ul><li>Thing 1</li> <li>Thing 2</li></ul>,
-// document.getElementById("root")
-// )
+ReactDOM.render(
+<ul><li>Thing 1</li> <li>Thing 2</li></ul>,
+document.getElementById("root")
+)
