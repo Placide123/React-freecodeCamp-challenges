2c4b72b71ca5a8e0d0f62faa71ac6857b9f5ca8e
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 08:41:21 +0200

    use imperative way

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-
-function MainComponent(){
-    return(
-        <h1>I'm learning REACT</h1>
-    )
-    
-}
-
-ReactDOM.render(
-    <div>
-        <MainComponent />
-    </div>,
-    document.getElementById("root")
-)
+//steps
+//step1: create a new h1 element
+//step2: give it some textContent
+//step3: give it a class name of "header"
+//step4: append it as a child of the div#root
+const h1= document.createElement('h1')
+h1.textContent="This is an imperative way to program "
+h1.className="header"
+document.getElementById("root").append(h1)
