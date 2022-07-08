70702aad7ff1628193caaf8e0e90424be9b18021
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 09:01:17 +0200

    showing how to render two element

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-//steps
-//step1: create a new h1 element
-//step2: give it some textContent
-//step3: give it a class name of "header"
-//step4: append it as a child of the div#root
-const h1= document.createElement('h1')
-h1.textContent="This is an imperative way to program "
-h1.className="header"
-document.getElementById("root").append(h1)
+const page = (
+<div>
+        <h1 className="header">
+            This is JSX
+        </h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+console.log(page)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
