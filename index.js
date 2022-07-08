18b7347ec397da67a8fa45e2c7e825524ea02cc2
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 16:28:03 +0200

    create a parent and child component

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 
-function Page(){
+function Header(){
     return(
-<div>
-    <header>
+        <header>
         <nav>
             <img src="react-2.svg" width="50px"></img>
         </nav>
     </header>
-    <h1>Reasons I'm excited to learn React</h1>
+    )
+}
+function Footer(){
+    return(
+        <footer>
+        <small>&copy;2022 Placide development. All rights reserved.</small>
+    </footer>
+
+    )
+}
+function MainContent(){
+return(
+    <div>
+        <h1>Reasons I'm excited to learn React</h1>
     <ol>
         <li>It is a popular library,so i will be able to fit in with the cool kids!</li>
         <li>I'm more likely to det a job as a developer if i know React</li>
         
     </ol>
+    </div>
+)
+}
+function Page(){
+    return(
+<div>
+   <Header />
+    <MainContent/>
 
-    <footer>
-        <small>&copy;2022 Placide development. All rights reserved.</small>
-    </footer>
-
+   <Footer />
 </div>
 
     )
 }
 
-ReactDOM.render(<Page/>, document.getElementById('root'))
+ReactDOM.render(<Page />, document.getElementById('root'))
