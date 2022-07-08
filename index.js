73b451475e7fb8b83d5ae2c9a53b456d5844f312
TWIRINGIYIMANA Placide <86245638+Placide123@users.noreply.github.com>
2022-07-08 07:58:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
 
 ReactDOM.render(<p>Hi, my name is Placide!</p>,document.getElementById("root"))
-// ReactDOM.render(
-// <ul><li>Thing 1</li> <li>Thing 2</li></ul>,
-// document.getElementById("root")
-// )
