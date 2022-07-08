530e9c62371643e5b51464cec2b0cd097e232141
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 14:56:34 +0200

    custom component part 2

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+
 function Page(){
     return(
-
+<div>
+    <header>
+        <nav>
+            <img src="react-2.svg" width="50px"></img>
+        </nav>
+    </header>
+    <h1>Reasons I'm excited to learn React</h1>
     <ol>
-        <li>get knowledge</li>
-        <li>designing web</li>
-        <li>it is soft, you can learn it</li>
+        <li>It is a popular library,so i will be able to fit in with the cool kids!</li>
+        <li>I'm more likely to det a job as a developer if i know React</li>
+        
     </ol>
+
+    <footer>
+        <small>&copy;2022 Placide development. All rights reserved.</small>
+    </footer>
+
+</div>
 
     )
 }
