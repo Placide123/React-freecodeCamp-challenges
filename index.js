c25a29bc3bacab4ad94e7de62c4377cc63c8324d
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-08 07:47:24 +0200

    This contain a helloword message

---
+++ index.js
@@ -0,0 +1,6 @@
+ReactDOM.render(<h1>hello, everyone!</h1>,document.getElementById("root"))
+// ReactDOM.render(<p>Hi, my name is Placide!</p>,document.getElementById("root"))
+// ReactDOM.render(
+// <ul><li>Thing 1</li> <li>Thing 2</li></ul>,
+// document.getElementById("root")
+// )
