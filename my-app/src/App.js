9387bd52400d5ba70637db3abb14d22d8b24a589
Placide123 <placidetwiringiyimana@outlook.com>
2022-07-09 16:06:03 +0200

    create a first react element and make a static web

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Main from './components/main'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="container">
+     <Navbar/>
+     <Main/>
+    </div>
+  );
+}
+
+export default App;
